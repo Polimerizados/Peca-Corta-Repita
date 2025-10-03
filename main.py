f52c70cf6a2a9c16ffbaae007211131968070232
怜王 <ritchielly1213@gmail.com>
2025-10-03 16:41:07 -0300

    Bug fix + Pac-Man

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ while True:
     screen.blit(main_menu, (0, 0))
 
     for event in pygame.event.get():
-        if event.type == KEYDOWN: # Teclado
+        if event.type == KEYDOWN: # Teclado 
 
             if event.key == K_ESCAPE: # Esc
                 pygame.quit()
@@ -38,6 +38,6 @@ while True:
 
             if event.key == K_SPACE: # Espaço
                 abrir_menu(screen, clock)
-                              
+                            
     pygame.display.update()
     clock.tick(60)
